b493876de073f8540eafe162a9076f5aeceaabab
Xavier Vila <xavi.vila@gmail.com>
2020-06-05 13:00:52 +0200

    update container names

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker-compose -f docker-compose-dbs.yml up -d
-docker-compose -f packages/hydra/hydra-dev.yml up -d
+docker-compose -f packages/hydra/hydra-prod.yml up -d
 docker-compose -f packages/identity-consent/docker-compose.yml up -d --build
--- packages/identity-consent/src/config.ts
+++ packages/identity-consent/src/config.ts
@@ -1,10 +1,10 @@
 const PORT = 3000;
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://dev.api.vidchain.net'
 const SIOP_PATH = '/siop'
 const RETURN_CALL = '/responses'
 
-const SIGNATURES = "https://walletapi-dev.vidchain.net/api/v1/signatures"
-const SIGNATURE_VALIDATION = "https://walletapi-dev.vidchain.net/api/v1/signature-validations";
+const SIGNATURES = "https://dev.api.vidchain.net/api/v1/signatures"
+const SIGNATURE_VALIDATION = "https://dev.api.vidchain.net/api/v1/signature-validations";
 
 //For now hardcoding enterprise wallet
 const authZToken = "eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QiLCJraWQiOiJ2aWRjaGFpbi1hcGkifQ.eyJzdWIiOiJWSURURVNUIElOQy4iLCJkaWQiOiJkaWQ6dmlkOjB4ZTZiNjk2RTJGNEQyYjM2M0EwMjYxNThENTBFM0E4NTlEMTI3MDZlZiIsIm5vbmNlIjoiei0wNDI3ZGMyNTE2ZDAiLCJpYXQiOjE1OTExMDE1NzcsImV4cCI6MTU5MzY5MzU3NywiYXVkIjoidmlkY2hhaW4tYXBpIn0.fS5WRFC2CJ0bCPxVLlDRVcWv4ZyJdfYu1P42xSkH678W7U1EzrlVATusBOSRyC7wRCIdqp_pQ5UQSlS9r7K0Kg";
