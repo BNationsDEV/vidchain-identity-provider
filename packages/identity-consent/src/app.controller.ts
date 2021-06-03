e26fc8fccb52c56d65f7f9fd0398d8391afc2d0c
Alex Bean <alexfraga10@gmail.com>
2021-06-03 11:49:14 +0200

    fix: serve json for ios deep links

--- packages/identity-consent/src/app.controller.ts
+++ packages/identity-consent/src/app.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Req, Res, Post, Query } from "@nestjs/common";
 import { Response, Request } from "express";
 import AppService from "./app.service";
 
+import jsonFile from "./templates/apple-app-site-association.json";
+
 @Controller()
 export default class AppController {
   constructor(private readonly appService: AppService) {}
@@ -32,5 +34,11 @@ export default class AppController {
   @Post("consent")
   doConsent(@Req() req: Request, @Res() res: Response): void {
     return this.appService.doConsent(req, res);
+  }
+
+  @Get("apple-app-site-association")
+  appleJSON(@Req() req: Request, @Res() res: Response): void {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(jsonFile));
   }
 }
