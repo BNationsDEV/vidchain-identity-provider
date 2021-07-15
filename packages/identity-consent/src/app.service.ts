534d6003c10b9e99f8a43e5e062177a63d3c9d07
Mauro Lucchini <maurolucchinidepompa@gmail.com>
2021-07-15 09:16:33 +0200

    refactor: email in context

--- packages/identity-consent/src/app.service.ts
+++ packages/identity-consent/src/app.service.ts
@@ -21,8 +21,7 @@ function processAccessGranted(challenge: string, body: DoLogin, res: Response) {
       // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
       // In our case DID
       subject: body.did, // 1
-
-      email: decodedJwt.payload.email || "test@test.com", // if scope contains email && body.jwt contains email body.email vs body.jwt.email
+      // if scope contains email && body.jwt contains email body.email vs body.jwt.email
 
       // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
       // set the "skip" parameter in the other route to true on subsequent requests!
@@ -39,6 +38,7 @@ function processAccessGranted(challenge: string, body: DoLogin, res: Response) {
       // data.
       context: {
         jwt: body.jwt, // 2
+        email: (decodedJwt.payload.email as string) || "test@test.com",
       },
     })
     .then((response) => {
